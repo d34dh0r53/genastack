e26da9969c44c9abf607096dfe35560dd24128f9
Kevin Carter <kevin.carter@rackspace.com>
2014-04-07 19:22:09 -0500

    added system check for config file

--- genastack/common/system_config.py
+++ genastack/common/system_config.py
@@ -59,26 +59,6 @@ class ConfigurationSetup(object):
             LOG.error(msg)
             raise genastack.CantContinue(msg)
 
-        self.check_perms()
-
-    def check_perms(self):
-        """Check the permissions of the config file.
-
-        Permissions must be 0600 or 0400.
-        """
-        # If config file is specified, confim proper permissions
-        if os.path.isfile(self.config_file):
-            confpath = self.config_file
-            if os.path.isfile(os.path.realpath(confpath)):
-                mode = oct(stat.S_IMODE(os.stat(confpath).st_mode))
-                if not any([mode == '0600', mode == '0400']):
-                    raise SystemExit(
-                        'To use a configuration file the permissions'
-                        ' need to be "0600" or "0400"'
-                    )
-        else:
-            LOG.error('Config file %s not found,' % self.config_file)
-
     def config_args(self, section='default'):
         """Loop through the configuration file and set all of our values.
 
