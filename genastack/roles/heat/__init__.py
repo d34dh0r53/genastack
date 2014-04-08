e440e4f4c1470efcd3e97ead7fd55697642633aa
Kevin Carter <kevin.carter@rackspace.com>
2014-04-08 00:06:05 -0500

    first commit

---
+++ genastack/roles/heat/__init__.py
@@ -0,0 +1,85 @@
+# =============================================================================
+# Copyright [2013] [Kevin Carter]
+# License Information :
+# This software has no warranty, it is provided 'as is'. It is your
+# responsibility to validate the behavior of the routines and its accuracy
+# using the code provided. Consult the GNU General Public license for further
+# details (see GNU General Public License).
+# http://www.gnu.org/licenses/gpl.html
+# =============================================================================
+from genastack.common import system_config
+
+
+CONFIG = system_config.ConfigurationSetup()
+ARGS = CONFIG.config_args(section='heat')
+BRANCH = ARGS.get('branch', 'master')
+PROJECT_URL = ARGS['project_url']
+
+
+BUILD_DATA = {
+    'heat': {
+        'use_system_python': ARGS.get('use_system_python', False),
+        'help': 'Install Heat from upstream on Branch "%s"' % BRANCH,
+        'required': [
+            'python',
+            'heat_client',
+            'keystone_client',
+            'ceilometer_client',
+            'trove_client'
+        ],
+        'directories': [
+            {
+                'path': '/var/log/heat',
+                'user': 'heat',
+                'group': 'heat',
+                'mode': '0755'
+            },
+            {
+                'path': '/var/lib/heat',
+                'user': 'heat',
+                'group': 'heat',
+                'mode': '0755'
+            },
+            {
+                'path': '/etc/heat',
+                'user': 'heat',
+                'group': 'heat',
+                'mode': '0755'
+            }
+        ],
+        'group_create': [
+            {
+                'group': 'heat',
+                'system': True
+            }
+        ],
+        'user_create': [
+            {
+                'user': 'heat',
+                'group': 'heat',
+                'home': '/var/lib/heat',
+                'system': True
+            }
+        ],
+        'git_install': [
+            {
+                'name': 'heat',
+                'project_url': PROJECT_URL,
+                'branch': BRANCH,
+                'config_example': 'etc/heat=/etc/heat',
+                'group_owner': 'heat',
+                'user_owner': 'heat',
+                'mode': '0644'
+            }
+        ],
+        'pip_install': [
+            'mysql-python',
+            'pbr',
+            'qpid-python'
+        ],
+        'apt_packages': [
+            'mysql-client',
+            'libmysqlclient-dev'
+        ]
+    }
+}
