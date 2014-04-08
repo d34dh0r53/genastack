0446aaea401f464d8deb9a432141c32dcbe3da1b
Kevin Carter <kevin.carter@rackspace.com>
2014-04-08 00:06:10 -0500

    first commit

---
+++ genastack/roles/ceilometer_client/__init__.py
@@ -0,0 +1,35 @@
+# =============================================================================
+# Copyright [2013] [Kevin Carter]
+# License Information :
+# This software has no warranty, it is provided 'as is'. It is your
+# responsibility to validate the behavior of the routines and its accuracy
+# using the code provided. Consult the GNU General Public license for further
+# details (see GNU General Public License).
+# http://www.gnu.org/licenses/gpl.html
+# =============================================================================
+from genastack.common import system_config
+
+
+CONFIG = system_config.ConfigurationSetup()
+ARGS = CONFIG.config_args(section='ceilometer_client')
+BRANCH = ARGS.get('branch', 'master')
+PROJECT_URL = ARGS['project_url']
+
+
+BUILD_DATA = {
+    'ceilometer_client': {
+        'use_system_python': ARGS.get('use_system_python', False),
+        'python_venv': {
+            'name': 'ceilometer'
+        },
+        'help': 'Install Ceilometer-Client from upstream, Branch "%s"'
+                % BRANCH,
+        'git_install': [
+            {
+                'name': 'ceilometer_client',
+                'project_url': PROJECT_URL,
+                'branch': BRANCH
+            }
+        ],
+    }
+}
